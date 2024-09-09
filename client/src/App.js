ccf98cf0b559eafa9a8a4e2f8e800d8ae7488df3
Andreas Skotadis <skottydeveloper@gmail.com>
2024-09-10 01:18:47 +1000

    Replace all instances of the default React icon with the EmoteLog app icon.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import logo from './assets/logo.png';
 import './App.css';
 import SplashScreen from './components/SplashScreen';
 
