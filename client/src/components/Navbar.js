e5a397135dacd3963a0eb4c0e1f0ed89208de908
Andreas Skotadis <skottydeveloper@gmail.com>
2024-09-12 19:59:59 +1000

    Create CSS file dedicated to navigation bar component.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,3 +1,4 @@
+import './Navbar.css';
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 export default function Navbar() {
@@ -18,12 +19,12 @@ export default function Navbar() {
 }
 
 function CustomLink({ to, children, ...props }) {
-    const resolvedPath = useResolvedPath(to)
-    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+    const resolvedPath = useResolvedPath(to);
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true });
 
     return (
         <li className={isActive ? "active" : ""}>
             <Link to={to} {...props}>{children}</Link>
         </li>
-    )
+    );
 }
