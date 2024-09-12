8bb563a9cb4b7c1057a6c078db1bfbebb2f1e9d8
Andreas Skotadis <skottydeveloper@gmail.com>
2024-09-12 20:40:02 +1000

    Convert static navigation bar, into offcanvas navigation bar with hamburger icon.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,21 +1,31 @@
-import './Navbar.css';
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import { Navbar, Nav, Offcanvas, Container } from 'react-bootstrap';
+import './Navbar.css';
 
-export default function Navbar() {
+export default function CustomNavbar() {
     return (
-        <nav>
-            <Link to="/">Home</Link>
-
-            <ul>
-                <CustomLink to="/daily-view">Daily View</CustomLink>
-                <CustomLink to="/calendar">Calendar</CustomLink>
-                <CustomLink to="/help">Help</CustomLink>
-                <CustomLink to="/history">History</CustomLink>
-                <CustomLink to="/profile">Profile</CustomLink>
-                <CustomLink to="/settings">Settings</CustomLink>
-            </ul>
-        </nav>
-    )
+        <Navbar bg="light" expand={false} className="mb-3">
+            <Container fluid>
+                <Navbar.Brand as={Link} to="/">Navbar Offcanvas</Navbar.Brand>
+                <Navbar.Toggle aria-controls="offcanvasNavbar" />
+                <Navbar.Offcanvas id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" placement="end">
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas Menu</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <Nav className="justify-content-end flex-grow-1 pe-3">
+                            <CustomLink to="/daily-view">Daily View</CustomLink>
+                            <CustomLink to="/calendar">Calendar</CustomLink>
+                            <CustomLink to="/help">Help</CustomLink>
+                            <CustomLink to="/history">History</CustomLink>
+                            <CustomLink to="/profile">Profile</CustomLink>
+                            <CustomLink to="/settings">Settings</CustomLink>
+                        </Nav>
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
+            </Container>
+        </Navbar>
+    );
 }
 
 function CustomLink({ to, children, ...props }) {
@@ -23,8 +33,8 @@ function CustomLink({ to, children, ...props }) {
     const isActive = useMatch({ path: resolvedPath.pathname, end: true });
 
     return (
-        <li className={isActive ? "active" : ""}>
-            <Link to={to} {...props}>{children}</Link>
-        </li>
+        <Nav.Link as={Link} to={to} className={isActive ? "active" : ""} {...props}>
+            {children}
+        </Nav.Link>
     );
 }
