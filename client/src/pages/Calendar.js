6d45130164128ce7091697b7e3c6cf7cedfcfc9c
Pulkit9918 <61176817+Pulkit9918@users.noreply.github.com>
2024-09-21 16:45:32 +1000

    Calender Screen Test - 1

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -1,3 +1,5 @@
-export default function Calendar() {
-    return <h1>Calendar</h1>
-}
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import './calender.css'; // Ensure this CSS file exists for styling
+
+export default Calender;
