2cab625b4daa309fa043e92866f7742a67688585
Pulkit9918 <61176817+Pulkit9918@users.noreply.github.com>
2024-09-29 19:30:20 +1000

    Login Fix -2

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -11,7 +11,7 @@ const translations = {
     Chinese: { username: "用户名", password: "密码", login: "登录", signUp: "注册", forgotPassword: "忘记密码？" }
 };
 
-export default function Login({ onLogin, language = "English" }) {
+export default function Login({ onLogin, language, setLanguage }) {
 
     const t = translations[language];
 
