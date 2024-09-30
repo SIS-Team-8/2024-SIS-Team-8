a95344d6438327615680080a6ac1593f3b3ca894
Pulkit9918 <61176817+Pulkit9918@users.noreply.github.com>
2024-09-30 20:33:18 +1000

    Sign up Fix

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -3,7 +3,7 @@ import { useNavigate, Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 import './SignUp.css';
 
-export default function SignUp() {
+export default function SignUp({language, theme }) {
     const navigate = useNavigate();
 
     const handleSignUpClick = () => {
@@ -11,24 +11,24 @@ export default function SignUp() {
     };
 
     return (
-        <div id="signup-container">
+        <div id="signup-container" className={theme}>
             <img src={logo} alt="App Logo" id="logo" />
 
             <div id="inputBox">
                 <form>
-                    <input id="emailBox" placeholder="Email" type="email"></input>
+                    <input id="emailBox" placeholder="Email" type="email" className={theme}></input>
                     <p></p>
-                    <input id="userBox" placeholder="Username"></input>
+                    <input id="userBox" placeholder="Username" className={theme}></input>
                     <p></p>
-                    <input id="passBox" placeholder="Password" type="password"></input>
+                    <input id="passBox" placeholder="Password" type="password" className={theme}></input>
                     <p></p>
-                    <input id="passBox" placeholder="Confirm Password" type="password"></input>
+                    <input id="passBox" placeholder="Confirm Password" type="password" className={theme}></input>
                 </form>
 
                 <button id="button" onClick={handleSignUpClick}>Create Account</button>
 
                 <p id="bottomText">
-                    <Link to="/login" id="link">Login</Link>
+                    <Link to="/login" id="link" className={theme}>Login</Link>
                 </p>
             </div>
         </div>
