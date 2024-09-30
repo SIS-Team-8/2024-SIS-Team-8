fada4664d075d5f00f7d10a7005f4e3b36b9c273
Pulkit9918 <61176817+Pulkit9918@users.noreply.github.com>
2024-10-01 01:28:12 +1000

    History Fix-13

--- client/src/components/BarChart.js
+++ client/src/components/BarChart.js
@@ -40,11 +40,11 @@ const BarChartComponent = ({ data, xAxisLabel, yAxisLabel, tooltipText, barColor
 
     const translatedData = defaultEmoteData.map((item) => ({
         ...item,
-        Angry: t[item.Angry] || item.Angry, // Replace the `name` property with the translated label if it exists, else keep the original
-        Sad: t[item.Sad] || item.Sad,
-        Happy: t[item.Happy] || item.Happy,
-        Bored: t[item.Bored] || item.Bored,
-        Scared: t[item.Scared] || item.Scared
+        Angry: t.Angry || item.Angry, // Replace the `name` property with the translated label if it exists, else keep the original
+        Sad: t.Sad || item.Sad,
+        Happy: t.Happy || item.Happy,
+        Bored: t.Bored || item.Bored,
+        Scared: t.Scared || item.Scared
     }));
 
     return ( 
