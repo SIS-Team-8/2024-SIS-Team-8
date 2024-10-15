7fcfe93d293a376cba3173056909639267c674e6
Alexandar Nikolic <alexandar.nikolic2@gmail.com>
2024-10-15 22:33:43 +1100

    added final images for calendar and daily view

--- client/src/pages/OnboardingOverview.js
+++ client/src/pages/OnboardingOverview.js
@@ -5,15 +5,17 @@ import logDailyEmotion from '../assets/log-daily-emotion.png';
 import moodLogging from '../assets/mood-logging.png';
 import menu from '../assets/menu.png';
 import history from '../assets/history.png';
+import calendar from '../assets/calendar.png';
+import dailyView from '../assets/daily-view.png';
 
 export default function OnboardingOverview() {
     // Array of images and paragraphs to cycle through
-    const images = [logDailyEmotion, moodLogging, menu, , ,history];
+    const images = [logDailyEmotion, moodLogging, menu, calendar, dailyView, history];
     const paragraphs = [
         "To begin your journaling select Log Daily Emotion",
         "Select how you are feeling and submit the entry. If you choose you can add a custom note and upload an image. Entries can be edited at any time",
         "Other features such as the calendar or history page can be accessed through the menu on the top right corner",
-        "The calendar allows you to view your past entries through each month",
+        "The calendar allows you to view your past entries through each month or year",
         "Selecting an entry from the calendar brings you to the daily view which allows you to view any notes or images inputed into the entry",
         "History allows you to view the frequency of your emotions in a particular week, month or year"
     ];
