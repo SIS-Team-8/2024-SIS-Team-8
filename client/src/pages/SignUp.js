4c34544c8f737b279e5207e8e1ea897437010a4b
Alexandar Nikolic <alexandar.nikolic2@gmail.com>
2024-10-20 13:21:47 +1100

    small adjustments to sign up

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,4 +1,4 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-toastify";
