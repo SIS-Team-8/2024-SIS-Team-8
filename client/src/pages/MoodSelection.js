1de87ea9bac2818340893064c8be3e7ed3531f7b
Pulkit9918 <61176817+Pulkit9918@users.noreply.github.com>
2024-10-25 00:10:42 +1100

    Ishaan Changes-3

--- client/src/pages/MoodSelection.js
+++ client/src/pages/MoodSelection.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import './MoodSelection.css'
 import veryAngry from '../assets/emoji/very-angry.png'
 import sad from '../assets/emoji/sad.png'
