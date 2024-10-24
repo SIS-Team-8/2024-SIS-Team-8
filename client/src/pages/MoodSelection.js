a7558fa266cb8daf30af80b6a64a995169201a6f
Pulkit9918 <61176817+Pulkit9918@users.noreply.github.com>
2024-10-24 14:53:40 +1100

    Changes-23

--- client/src/pages/MoodSelection.js
+++ client/src/pages/MoodSelection.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import './MoodSelection.css'
 import veryAngry from '../assets/emoji/very-angry.png'
 import sad from '../assets/emoji/sad.png'
@@ -37,7 +37,6 @@ const translations = {
 };
 
 export default function MoodSelection({ language = "English", theme = "light", onMoodUpdate }) {
-    const location = useLocation();
     const [imageSrc, setImageSrc] = useState([]);  // Store sub-row images based on mood
     const [rowOpacity, setRowOpacity] = useState(Array(5).fill(1));  // Set initial opacity of row images to 1
     const [subRowOpacity, setSubRowOpacity] = useState(Array(5).fill(1));  // Sub-row opacity starts at 1
@@ -58,11 +57,7 @@ export default function MoodSelection({ language = "English", theme = "light", o
     };
 
     const handleSubmit = () => {
-        onMoodUpdate(date, selectedMood, note);
-        // Optionally, clear the form or give feedback
-        alert('Mood updated successfully!');
-        setSelectedMood('');
-        setNote('');
+        onMoodUpdate(selectedMood, note);
     };
 
     const t = translations[language];
