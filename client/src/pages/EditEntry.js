93cf0f697b802bb9875f232000acdd56def3e17d
Pulkit9918 <61176817+Pulkit9918@users.noreply.github.com>
2024-10-24 23:17:35 +1100

    Changes-114

--- client/src/pages/EditEntry.js
+++ client/src/pages/EditEntry.js
@@ -123,7 +123,7 @@ export default function EditEntry({ language = "English", theme = "light", moodD
 
     const handleMoodChange = (mood) => {
         setSelectedMood(mood);
-        setSelectedSubMood(subImages);
+        setSelectedImages(subImages);
         setMoodIntensity(moodIntensityMap[mood] || 'N/A');
         setMoodImages(moods[mood].subImages);
     };
