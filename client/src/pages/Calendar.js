5e4d3dc55118126cd1a8f528f0c25df46dbe22d5
Pulkit9918 <61176817+Pulkit9918@users.noreply.github.com>
2024-10-24 16:03:30 +1100

    Changes-29

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -141,7 +141,7 @@ const translations = {
     }
 };
 
-const CalendarScreen = ({theme, language }) => {
+const CalendarScreen = ({theme, language, onUpdate }) => {
     const navigate = useNavigate();
     const [currentMonth, setCurrentMonth] = useState(new Date(2024, 9));
     const [isYearlyView, setIsYearlyView] = useState(false);
