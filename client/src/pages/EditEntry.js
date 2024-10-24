fcd382f25b797f36e468b9148c7d5bf769083e0c
Pulkit9918 <61176817+Pulkit9918@users.noreply.github.com>
2024-10-24 23:46:03 +1100

    Changes-145

--- client/src/pages/EditEntry.js
+++ client/src/pages/EditEntry.js
@@ -113,6 +113,7 @@ export default function EditEntry({ language = "English", theme = "light", moodD
         setSubRowOpacity(Array(5).fill(1));  // Reset sub-row images' opacity to 1
     };
 
+    // This function will be used to submit the new mood and note
     const handleSubmit = () => {
         onMoodUpdate(date, { mood: selectedMood, subMood: selectedSubMood, intensity: moodIntensity, notes: note });
         navigate(`/daily-view/${date}`);
@@ -120,7 +121,6 @@ export default function EditEntry({ language = "English", theme = "light", moodD
 
     const handleMoodChange = (mood) => {
         setSelectedMood(mood);
-        setMoodImages(moods[mood].subImages);
         setMoodIntensity(moodIntensityMap[mood] || 'N/A');
         setMoodImages(moods[mood].subImages);
     };
