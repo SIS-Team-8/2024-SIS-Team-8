dee950ab1647e069b64e34f309b5db02762e2bf8
Pulkit9918 <61176817+Pulkit9918@users.noreply.github.com>
2024-10-26 16:07:43 +1100

    {ulkit Fixes-22

--- client/src/pages/DailyView.js
+++ client/src/pages/DailyView.js
@@ -170,7 +170,7 @@ const DailyView = ({theme, language, moodData, updateEntry, deleteEntry}) => {
                 <h1 style={{ color: 'white' }}>{translatedHeader}</h1>
 
                 <div className="emoji">
-                    <img src={getMoodEmoji(moodEntry.subMood)} alt={moodEntry.mood}/>
+                    <img src={getMoodEmoji(moodEntry.mood)} alt={moodEntry.mood}/>
                 </div>
 
                 {isEditing ? (
@@ -182,7 +182,7 @@ const DailyView = ({theme, language, moodData, updateEntry, deleteEntry}) => {
                     </>
                 ) : (
                     <>
-                        <p className="intensity">{t.intensity} handleIntensityChange{intensity}</p>
+                        <p className="intensity">{t.intensity} {intensity}</p>
                         <p className="notes">{t.notes} {notes}</p>
                         <button className="edit-button" onClick={handleEdit}>
                             ✏ {t.editEntry}
