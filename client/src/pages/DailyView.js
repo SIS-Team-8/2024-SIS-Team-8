340a79b44ce38ea30a02989268539acfae2ce381
Pulkit9918 <61176817+Pulkit9918@users.noreply.github.com>
2024-10-26 16:10:49 +1100

    Pulkit Fixes-25

--- client/src/pages/DailyView.js
+++ client/src/pages/DailyView.js
@@ -182,7 +182,7 @@ const DailyView = ({theme, language, moodData, updateEntry, deleteEntry}) => {
                     </>
                 ) : (
                     <>
-                        <p className="intensity">{t.intensity} {moodEntry.Intensity}</p>
+                        <p className="intensity">{t.intensity} {moodEntry.intensity}</p>
                         <p className="notes">{t.notes} {notes}</p>
                         <button className="edit-button" onClick={handleEdit}>
                             ✏ {t.editEntry}
