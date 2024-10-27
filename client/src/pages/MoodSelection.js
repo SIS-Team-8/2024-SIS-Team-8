a36c67e661bb2cc49c897f4943f289046ec14413
Pulkit9918 <61176817+Pulkit9918@users.noreply.github.com>
2024-10-27 15:56:02 +1100

    Pulkit Changes-25

--- client/src/pages/MoodSelection.js
+++ client/src/pages/MoodSelection.js
@@ -130,7 +130,10 @@ export default function MoodSelection({ language = "English", theme = "light", m
     // This function will be used to submit the new mood and note
     const handleSubmit = () => {
         const date = new Date().toISOString().split('T')[0];
-        onMoodUpdate(date, { mood: selectedMood, intensity: moodIntensity, note });
+        //onMoodUpdate(date, { mood: selectedMood, intensity: moodIntensity, note });
+        const moodEntry = { mood: selectedMood, intensity: moodIntensity, note };
+
+        // Trigger the callback to pass mood to History page
         onMoodUpdate(date, moodEntry);
     };
 
