4e16523dfc4e95929087493f4dd72caad464236a
Alexandar Nikolic <alexandar.nikolic2@gmail.com>
2024-10-30 10:25:33 +1100

    removed un needed code

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from "axios";
-import { toast } from "react-toastify";
 import './Calendar.css';
 
 import angry from '../assets/emoji/angry.png';
