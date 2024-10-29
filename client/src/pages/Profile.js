7275fca54ded3d7dbcce1635e971ddaa7501460b
Andreas Skotadis <skottydeveloper@gmail.com>
2024-10-29 19:56:03 +1100

    Revert "Merge remote-tracking branch 'origin/Development' into andreas/history-full-stack"
    This reverts commit e1af93eee6a89cc17a3bb9c98bfe317b9e6799d7, reversing
    changes made to a6abb502dc67bd32aa6dcd63af3ccb04fb7e1643.

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -104,6 +104,7 @@ export default function Profile( { onLogout, theme, language}) {
                     address: res.data.user.address,
                     profile_pic: res.data.user.profile_pic
                 });
+                console.log(avatarPhoto);
                 if (res.data.user.profile_pic !== "") {   
                     const base64Data = res.data.user.profile_pic.split(',')[1];
                     const binaryString = window.atob(base64Data);
@@ -127,7 +128,7 @@ export default function Profile( { onLogout, theme, language}) {
                 <button id="profile-button" onClick={handleImageUpload}>
                     <img src={avatarPhoto} alt="Profile Avatar"/>
                 </button>
-                <input type="file" name="avatarPhoto" accept=".png, .jpeg, .jpg" ref={fileUploadRef} onChange={uploadImageDisplay} hidden/>
+                <input type="file" name="avatarPhoto" accept=".png, .jpeg" ref={fileUploadRef} onChange={uploadImageDisplay} hidden/>
             </div>
             <div className="profile-details">
                 {/* Name Field */}
